eaff282948a5b14fbe1791772468098bb630f367
Harsh1126 <harshb1508@gmail.com>
2018-10-26 11:18:21 +0530

    Add files via upload

---
+++ Scripts/Bottom.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bottom : MonoBehaviour {
+	public Image img;
+	public GameObject buttonPrefab;
+	public Transform buttonContainer;
+
+	private void Start(){
+		Sprite[] sprites = Resources.LoadAll<Sprite> ("Bottom");
+		foreach (Sprite s in sprites) {
+			GameObject go = Instantiate (buttonPrefab) as GameObject;
+			go.transform.SetParent (buttonContainer, false);
+			go.GetComponent<Image> ().sprite = s;
+
+			string sName = s.name;
+			go.GetComponent<Button> ().onClick.AddListener (() => OnButtonClick (sName));
+		}
+	}
+
+	public void OnButtonClick(string imgName){
+		img.sprite = Resources.Load<Sprite> ("Bottom/" + imgName);
+	}
+}
---
+++ Scripts/Top.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Top: MonoBehaviour {
+	public Image img;
+	public GameObject buttonPrefab;
+	public Transform buttonContainer;
+
+	private void Start(){
+		Sprite[] sprites = Resources.LoadAll<Sprite> ("Top");
+		foreach (Sprite s in sprites) {
+			GameObject go = Instantiate (buttonPrefab) as GameObject;
+			go.transform.SetParent (buttonContainer, false);
+			go.GetComponent<Image> ().sprite = s;
+
+			string sName = s.name;
+			go.GetComponent<Button> ().onClick.AddListener (() => OnButtonClick (sName));
+		}
+	}
+
+	public void OnButtonClick(string imgName){
+		img.sprite = Resources.Load<Sprite> ("Top/" + imgName);
+	}
+
+
+}
